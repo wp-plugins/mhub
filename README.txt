5bb3ce2dfb1df80412b09e60b844ad5a9a1e245c
shaun.trennery <shaun.trennery@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-17 19:34:36 +0000

    git-svn-id: https://plugins.svn.wordpress.org/mhub/trunk@573756 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,68 @@
+=== MHub ===
+Plugin Name: MHub
+Contributors: shaun.trennery, MHub
+Tags: mobi, mobile, south africa, responsive, free, mobilizing
+Requires at least: 2.8.3
+Tested up to: 3.3
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+MHub mobi site redirector for WordPress. Ignoring the potential of the mobile web is something today's business owner simply cannot afford.
+
+== Description ==
+
+The MHub WordPress plugin makes redirecting your WordPress site to your MHub site for all your mobile users nice and easy. Works will all WordPress themes.
+
+MHub.co.za Features:
+
+* Built for Mobile - lighting fast; great looking and easy to use: the 3 pillars of any successful mobi site.
+* Touch-to-call - Making it as easy as possible for customers to contact you.
+* Map and Directions - Direct new customers right to your front door.
+* Discoverable - Our SEO experts have pulled out all the stops ensuring search engines like Google and Bing just love your MHub mobi site.
+* Your Own QR Code - Your custom QR codes can be downloaded in vector, png, or jpeg formats. Perfect for menus, brochures, shop windows, ads.
+* Mobile Analytics - Keep track of all your mobi visitors and potential customers.
+* Hosted - Never worry about monthly hosting fees, bandwidth usage, calling a help-desk blah blah. We got you covered.
+
+== Installation ==
+
+*I want to create a mobile website
+
+1. Go to mhub.co.za
+2. Search for your company
+3. Select your company from the results or skip ahead to step 2
+4. Confirm your company details and press submit
+5. Either login or register to link your new mobi to your account
+6. View and edit your new mobi on your MHub dashboard
+
+*I have a MHub mobi and want to redirect my WordPress site
+
+1. Go to your WordPress control panel
+2. Click "Plugins" and then "add new"
+3. Enter "MHub" as a search term and click "Search Plugins"
+4. Click install
+5. Click "Activate Plugin"
+6. Update MHub settings with the MHub slug you want mobile visitors to be redirected to.
+
+* Manual Install
+1. Extract the contents of mhub_wp.zip and upload to the wp-content/plugins folder.
+2. Activate the plugin through the "Plugins" menu in WordPress
+3. Update MHub settings with the MHub slug you want mobile visitors to be redirected to.
+
+Test by going to your site on your phone
+
+== Frequently Asked Questions ==
+
+= What is a mobi site? =
+Mobile sites are designed for the small screen, with the needs of mobile users in mind. A mobile-friendly site can help your business connect with customers and increase sales, but a bad mobile experience can drive your customers to your competition.
+
+= What is a mobi site redirect? =
+The MHub WordPress plugin removes this hassle and automatically redirects your WordPress site to your MHub mobi site when someone visits from their phone.
+
+= What if a mobile visitor navigates to my site from MHub, will they be redirected back to MHub? =
+No, links to your website on your mobi contain a special token which will prevent any additional mobile redirections
+
+== Changelog ==
+
+= 1.0 =
+* Initial Release
---
+++ plugin.php
@@ -0,0 +1,115 @@
+<?php
+/*
+Plugin Name: MHub
+Plugin URI: http://mhub.co.za
+Description: MHub mobi site redirector for WordPress by MHub. Ignoring the potential of the mobile web is something today's business owner simply cannot afford.
+Version: 1.0.0
+Date: 2012, July 16th
+Author: Shaun Trennery
+Author URI: http://twitter.com/shauntrennery
+*/
+/*
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+define("MHUB_PLUGINFILE", WP_PLUGIN_DIR."/mhub/lib/mdetect.php");
+
+if (!class_exists("MHubPlugin")) {
+  class MHubPlugin {
+
+    function MHubPlugin() {
+      $this->addActions();
+      register_activation_hook(__FILE__, array($this, "activatePlugin"));
+      register_deactivation_hook(__FILE__, array($this, "deactivatePlugin"));
+    }
+
+    function addActions() {
+      add_action("admin_init", array(&$this, "adminInit"));
+      add_action("init", array(&$this, "init"));
+      add_action("admin_menu", array(&$this, "mhub_wp_add_menu"));
+    }
+
+    function activatePlugin() {
+      add_option("mhub_do_activation_redirect", true);
+    }
+
+    function deactivatePlugin() {
+
+    }
+
+    function adminInit() {
+      if(get_option("mhub_do_activation_redirect", false)) {
+        delete_option("mhub_do_activation_redirect");
+        wp_redirect("options-general.php?page=mhub/plugin.php");
+      }
+    }
+
+    function init() {
+      if(file_exists(MHUB_PLUGINFILE)) {
+        require_once(MHUB_PLUGINFILE);
+
+        if(!function_exists("wp_redirect")) {
+          require(ABSPATH . WPINC . "/pluggable.php");
+        }
+
+        $dontRedirect = false;
+        if(!is_bool(strpos($_SERVER["REQUEST_URI"], "no_redirect=true"))) $dontRedirect = true;
+        $targetUrl = "http://mhub.co.za/".get_option("mhub_wp_target_url");
+
+        if(($targetUrl != "") && ($this->isValidURL($targetUrl)) && (!$dontRedirect)) {
+          $uagent_obj = new uagent_info();
+          $detect_mobile = $uagent_obj->DetectMobileLong();
+
+          if (($detect_mobile == 1)) {
+            wp_redirect($targetUrl);
+            exit();
+          }
+        }
+      }
+    }
+
+    function isValidURL($url) {
+      return preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $url);
+    }
+
+    function mhub_wp_add_menu() {
+      add_options_page("MHub", "Mhub", 8, __FILE__, array($this, "admin_options_wp_mhub"));
+    }
+
+    function admin_options_wp_mhub() {
+      echo '
+        <div class="wrap">
+          <h2>MHub Settings</h2>
+          <p>This WordPress plugin will detect mobile devices and redirect your vistors to your MHub mobi site. For this plugin to work, please enter your MHub mobi slug below.</p>
+          <p>Need a mobi? Head over to <a href="http://mhub.co.za" target="_blank">MHub</a> and get your mobi on.</p>
+          <form method="post" action="options.php">
+            '.wp_nonce_field('update-options').'
+            <table class="form-table">
+              <tr valign="top">
+                <th scope="row"><label for="mhub_wp_target_url">MHub mobi slug</label></th>
+                <td><input type="text" name="mhub_wp_target_url" id="mhub_wp_target_url" value="'.get_option('mhub_wp_target_url').'" class="regular-text code" /></td>
+              </tr>
+            </table>
+            <input type="hidden" name="action" value="update" />
+            <input type="hidden" name="page_options" value="mhub_wp_target_url" />
+            <p class="submit"><input type="submit" name="Submit" value="Submit" class="button-primary" /></p>
+          </form>
+        </div>';
+    }
+  }
+}
+
+$MHubPlugin = new MhubPlugin();
+?>
